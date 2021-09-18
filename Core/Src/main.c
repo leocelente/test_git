622f7d58aac4fcd24d9139958d926a7c1a234f91
Leonardo Celente <leocelente@gmail.com>
2021-09-18 17:31:45 -0300

    mais um commit

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
