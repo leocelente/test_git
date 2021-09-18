2139d4edf54cbd8d8d6daf237492dead812ec524
Leonardo Celente <leocelente@gmail.com>
2021-09-18 16:29:07 -0300

    This is a commit

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -63,7 +63,7 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	// FUNCIONOU
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
